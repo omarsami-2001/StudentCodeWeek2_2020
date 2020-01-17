56848b4fa5b5e26aac3875bc9740dfaff73944c5
omarsami-2001 <samiom@sheridancollege.ca>
2020-01-17 11:50:31 -0500

    initial commit for new student code

---
+++ src/ca/sheridancollege/softwarefundamentals/week1/Student1.java
@@ -0,0 +1,16 @@
+package ca.sheridancollege.softwarefundamentals.week1;
+
+/**
+ *
+ * @author PC
+ */
+public class Student1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
